6989006a1d005788a39dbb011a8651471a14de08
YOHAN ALEK PLAZOLA ARANGURE <yh9dev@gmail.com>
2025-03-02 20:26:33 -0700

    Sintaxis básica

---
+++ php-basics/1_sintaxis-basica.php
@@ -0,0 +1,13 @@
+<?php
+
+# Etiquetas PHP              # Se desaconseja hacerlo de esta manera
+
+// <?php  Apertura              <?  Apertura
+// 
+//     codigo php...                codigo php...
+// 
+// ?>     Cierre                ?>     Cierre
+
+# Fuera de estas etiquetas todo se renderiza como HTML.
+# PHP requiere que cada instrucción se termine con punto y coma.
+# <?php echo 'Hola mundo' ?> En este caso no es necesario incluir el punto y coma, es implícito.
